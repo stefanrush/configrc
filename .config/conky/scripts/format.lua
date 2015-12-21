830a1db21aab5b478120cd7990d60df5d64de297
Stefan Rush <stefanrush@gmail.com>
2015-12-21 12:34:23 -0500

    Changed to links

--- .config/conky/scripts/format.lua
+++ .config/conky/scripts/format.lua
@@ -1,1 +1,7 @@
-/home/stefan/.config/conky/scripts/format.lua
+function conky_format_num( format, num )
+    return string.format( format, conky_parse( num ) )
+end
+
+function conky_upcase_str( str )
+    return string.upper( conky_parse( str ) )
+end
