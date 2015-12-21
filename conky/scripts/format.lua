ce9e84d718d8c7be409baae3ee945897a0f0ac60
Stefan Rush <stefanrush@gmail.com>
2015-12-21 12:25:32 -0500

    Changed files to symlinks

--- conky/scripts/format.lua
+++
@@ -1,7 +0,0 @@
-function conky_format_num( format, num )
-    return string.format( format, conky_parse( num ) )
-end
-
-function conky_upcase_str( str )
-    return string.upper( conky_parse( str ) )
-end
